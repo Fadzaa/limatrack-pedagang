767076c36111690ee727ff9e0a2f8d7a4c76ee94
Fadzaa <fadza20@gmail.com>
2024-04-01 21:00:11 +0700

    feat: [FA] add banner image, time picker, image picker, file picker

--- lib/app/pages/initial_pages/verfication_page/verification_page_controller.dart
+++ lib/app/pages/initial_pages/verfication_page/verification_page_controller.dart
@@ -1,8 +1,54 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class VerificationPageController extends GetxController {
   RxString dropdownValue = "Kudus".obs;
+
+  DateTime currentTime = DateTime.now();
+  RxString openTimeString = '07:00'.obs;
+  RxString closeTimeString = '21:00'.obs;
+
+
+  RxString filePathImage = ''.obs;
+  RxString filePathDocument = ''.obs;
+
+
+  void onTimePickerSelected(DateTime dateTime, RxString timeString) {
+    showTimePicker(
+      context: Get.context!,
+      initialTime: TimeOfDay.fromDateTime(dateTime),
+    ).then((value) {
+      if (value != null) {
+        dateTime = DateTime(
+          currentTime.year,
+          currentTime.month,
+          currentTime.day,
+          value.hour,
+          value.minute,
+        );
+        timeString.value = "${value.hour}:${value.minute}";
+      }
+
+    }
+    );
+  }
+
+  Future<void> pickFile(RxString filePath) async {
+    //if path image extension png jpg
+    //if path document pdf doc docx
+    final pickedFile = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'doc', 'docx'],
+    );
+
+    if (pickedFile != null) {
+      filePath.value = pickedFile.files.single.path!;
+    }
+
+
+  }
 
 
 
@@ -14,13 +60,13 @@ class VerificationPageController extends GetxController {
     }
   }
 
-
-
   void onChangeDropdown(String value, List<String> items) {
     dropdownValue.value = value;
     items.remove(value);
     items.insert(0, value);
   }
+
+
 
   @override
   void onInit() {
