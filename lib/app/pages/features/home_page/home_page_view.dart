b404a329fb37dcd91bade2942962f679e7a3398c
RadyaHarbani <radya.i4m@gmail.com>
2024-05-23 00:46:20 +0700

    feat : [RH] add draggable content

--- lib/app/pages/features/home_page/home_page_view.dart
+++ lib/app/pages/features/home_page/home_page_view.dart
@@ -13,50 +13,40 @@ import 'items/item_pesanan_vertical.dart';
 import 'model/pesanan_data.dart';
 
 class HomePageView extends GetView<HomePageController> {
-  HomePageView({
-    super.key,
-    this.user
-  });
+  HomePageView({super.key, this.user});
 
-   UserModel? user;
+  UserModel? user;
 
   @override
   Widget build(BuildContext context) {
-
     final userName = user!.namaLengkap;
 
     return Scaffold(
-      body:  SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    color: Colors.grey[200],
-                    height: MediaQuery.of(context).size.height * 0.7,
-                    child: GoogleMap(
-                        initialCameraPosition: const CameraPosition(
-                          target: LatLng(-6.966667, 110.416664),
-                          zoom: 15,
-                        ),
-                    )
-                  ),
-
-                  ContainerStatus(
-                      name: userName?? ""
-        )
-
-                ],
-              ),
-
-
-              const ContainerContent(),
-            ],
+        body: SafeArea(
+      child: Stack(
+        children: [
+          Container(
+              color: Colors.grey[200],
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: GoogleMap(
+                initialCameraPosition: const CameraPosition(
+                  target: LatLng(-6.966667, 110.416664),
+                  zoom: 15,
+                ),
+              )),
+          ContainerStatus(name: userName ?? ""),
+          DraggableScrollableSheet(
+            initialChildSize: 0.20,
+            minChildSize: 0.20,
+            maxChildSize: 1,
+            builder: (context, scrollableController) => SingleChildScrollView(
+              controller: scrollableController,
+              child: const ContainerContent(),
+            ),
           ),
-        ),
-      )
-    );
+        ],
+      ),
+    ));
   }
 }
 
@@ -70,94 +60,66 @@ class ContainerContent extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
       ),
-
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
           const Gap(20),
-
           Container(
               width: 50,
               height: 5,
               decoration: BoxDecoration(
                 color: const Color(0xFFD9D9D9),
                 borderRadius: BorderRadius.circular(20),
-              )
-
-          ),
-
+              )),
           const Gap(30),
-
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Flexible(
-                  child: Text.rich(
-                  TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "List ",
-                          style: tsTitleMedium.copyWith(
-                              fontWeight: FontWeight.w600,
-                              color: blackColor
-                          ),
-                        ),
-
-                        TextSpan(
-                          text: "Pesananmu\n",
-                          style: tsTitleMedium.copyWith(
-                              fontWeight: FontWeight.w600,
-                              color: primaryColor
-                          ),
-                        ),
-
-                        TextSpan(
-                          text: "Hari Ini ",
-                          style: tsTitleMedium.copyWith(
-                              fontWeight: FontWeight.w600,
-                              color: blackColor
-                          ),
-                        ),
-                      ]
-                  )
-                  )
-              ),
-
+                  child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "List ",
+                  style: tsTitleMedium.copyWith(
+                      fontWeight: FontWeight.w600, color: blackColor),
+                ),
+                TextSpan(
+                  text: "Pesananmu\n",
+                  style: tsTitleMedium.copyWith(
+                      fontWeight: FontWeight.w600, color: primaryColor),
+                ),
+                TextSpan(
+                  text: "Hari Ini ",
+                  style: tsTitleMedium.copyWith(
+                      fontWeight: FontWeight.w600, color: blackColor),
+                ),
+              ]))),
               Container(
                 height: 30,
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 decoration: BoxDecoration(
-                  color: secondaryColor,
-                  borderRadius: BorderRadius.circular(30)
-                ),
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.circular(30)),
                 child: Center(
-                  child: Text(formattedDate, style: tsBodySmall.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600
-                  )),
+                  child: Text(formattedDate,
+                      style: tsBodySmall.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.w600)),
                 ),
               )
             ],
           ),
-
           const Gap(20),
-
           ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               itemCount: pesanan_data.length,
-              itemBuilder: (context, index) =>
-                  ItemPesananVertical(
-                      name: pesanan_data[index].name,
-                      items: pesanan_data[index].items
-                  )
-          )
-
-
+              itemBuilder: (context, index) => ItemPesananVertical(
+                  name: pesanan_data[index].name,
+                  items: pesanan_data[index].items))
         ],
       ),
     );
@@ -165,165 +127,154 @@ class ContainerContent extends StatelessWidget {
 }
 
 class ContainerStatus extends GetView<HomePageController> {
-  const ContainerStatus({
-    super.key,
-    required this.name
-  });
+  const ContainerStatus({super.key, required this.name});
 
   final String name;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.7,
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3), // changes position of shadow
-                      ),
-                    ]
-                ),
-
-                child: Column(
-                  children: [
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Text("Hello,", style: tsBodyMedium.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: primaryColor
-                            ),
-                            ),
-
-                            const SizedBox(height: 10,),
-
-                            Text(" $name👋", style: tsBodyMedium),
-                          ],
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.7,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(0, 3), // changes position of shadow
                         ),
-
-
-                        Container(
-                          height: 20,
-                          width: 20,
-                          decoration: const BoxDecoration(
-                              color: Colors.black,
-                              shape: BoxShape.circle
-                          ),
-
-                          child: const Center(child: Icon(Icons.question_mark, color: Colors.white, size: 16,),),
-                        )
-                      ],
-                    ),
-
-                    const Gap(15),
-
-                    Container(
-                      height: 50,
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
-                      decoration: BoxDecoration(
-                        color: successColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-
-                      child: Row(
+                      ]),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          const Icon(Icons.location_pin, color: successColor, size: 20,),
-
-                          const Gap(10),
-
-                          Text("Kamu Berada di Zona Aman", style: tsBodySmall.copyWith(
-                              fontWeight: FontWeight.w500,
-                              color: successColor
-                          ),)
-
-
+                          Row(
+                            children: [
+                              Text(
+                                "Hello,",
+                                style: tsBodyMedium.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: primaryColor),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(" $name👋", style: tsBodyMedium),
+                            ],
+                          ),
+                          Container(
+                            height: 20,
+                            width: 20,
+                            decoration: const BoxDecoration(
+                                color: Colors.black, shape: BoxShape.circle),
+                            child: const Center(
+                              child: Icon(
+                                Icons.question_mark,
+                                color: Colors.white,
+                                size: 16,
+                              ),
+                            ),
+                          )
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const Gap(20),
-
-              InkWell(
-                onTap: () => statusDaganganBottomSheet(controller),
-                child: Container(
-                  height: 35,
-                  width: Get.width * 0.4,
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-
-                  child: Row(
-                    children: [
+                      const Gap(15),
                       Container(
-                        width: 10,
-                        height: 10,
+                        height: 50,
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 14, vertical: 12),
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: successColor,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 7,
-                                offset: const Offset(0, 3), // changes position of shadow
-                              ),
-                            ]
+                          color: successColor.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.location_pin,
+                              color: successColor,
+                              size: 20,
+                            ),
+                            const Gap(10),
+                            Text(
+                              "Kamu Berada di Zona Aman",
+                              style: tsBodySmall.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: successColor),
+                            )
+                          ],
+                        ),
                       ),
-
-                      const Gap(10),
-
-                      Text("Warung Buka", style: tsBodySmall.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),)
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
-
-          const CommonWarningBox(text: "Sekarang adalah waktu kamu tutup, status akan dirubah dalam 5 detik", colorType: dangerColor,)
-        ],
-      )
-    );
+                const Gap(20),
+                InkWell(
+                  onTap: () => statusDaganganBottomSheet(controller),
+                  child: Container(
+                    height: 35,
+                    width: Get.width * 0.4,
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 10,
+                          height: 10,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: successColor,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 7,
+                                  offset: const Offset(
+                                      0, 3), // changes position of shadow
+                                ),
+                              ]),
+                        ),
+                        const Gap(10),
+                        Text(
+                          "Warung Buka",
+                          style: tsBodySmall.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const CommonWarningBox(
+              text:
+                  "Sekarang adalah waktu kamu tutup, status akan dirubah dalam 5 detik",
+              colorType: dangerColor,
+            )
+          ],
+        ));
   }
 }
-
-
-
-
-
-
-
-
-
